5016b4027a55efaab3b23f2e30548e67bb6ed945
Hello <ivankov1823202@gmail.com>
2023-03-04 21:42:35 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+def read_file(file_name):
+    with open(file_name, encoding='utf-8') as file:
+        data = file.readlines()
+        rows = len(data)
+        dict1[file.name] = rows
+        return dict1
+
+dict1 = {}
+read_file('1.txt')
+read_file('2.txt')
+read_file('3.txt')
+sorted_tuples = sorted(dict1.items(), key=lambda item: item[1])
+sorted_dict = {k: v for k, v in sorted_tuples}
+
+with open('result.txt', 'w', encoding='utf-8') as file:
+    text = ''
+    for key, value in sorted_dict.items():
+        text += key + '\n'
+        text += str(value) + '\n'
+        with open(key, encoding='utf-8') as f:
+            content = f.readlines()
+        text += "".join(content)
+        text += '\n'
+    file.write(text)
